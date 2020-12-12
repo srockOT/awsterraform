9c0d75627f8b7c94568df44b1e0abe2753f0cb9f
srock <srock@opentext.com>
2020-12-12 14:52:52 -0500

    mid chapter 4

--- prod/services/webserver-cluster/outputs.tf
+++ prod/services/webserver-cluster/outputs.tf
@@ -3,3 +3,4 @@ output "alb_dns_name" {
     description     = "The domain name of the load balance"
 }
 
+
--- prod/services/webserver-cluster/variables.tf
+++ prod/services/webserver-cluster/variables.tf
@@ -7,11 +7,11 @@ variable "db_remote_state_bucket" {
 variable "db_remote_state_key" {
     description  = "The name of the key in the S3 bucket used for the database's remote state storage"
     type = string 
-    default = "qa/data-stores/mysql/terraform.tfstate"
+    default = "prod/data-stores/mysql/terraform.tfstate"
 }
 
 variable "cluster_name" {
     description = "The name to use to namespace all the resources in the cluster"
     type = string
-    default = "webservers-qa"
+    default = "webservers-prod"
 }
